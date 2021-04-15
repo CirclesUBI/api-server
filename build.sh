214e8735009da8402b9a13605c5643ed19b07f21
jaensen <daniel@djanz.de>
2021-04-15 05:57:10 +0200

    updated all package versions and removed one unnecessary dependency (ix)

--- build.sh
+++ build.sh
@@ -4,6 +4,10 @@ echo "Installing dependencies .."
 npm i
 
 echo "Building 'omo-central-server' .."
+rm -r -f dist
 npm run graphql-generate
 npx --no-install prisma generate --schema=./schema_template.prisma || exit
+mkdir dist
+mkdir dist/auth-client
+cp -f src/auth-client/* dist/auth-client
 npx --no-install tsc || exit
