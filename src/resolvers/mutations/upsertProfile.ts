2ef5898bd59a5638be04a3df84a27cd3fa396696
jaensen <daniel@djanz.de>
2021-05-12 03:39:47 +0200

    don't allow updates of profiles other than the own

--- src/resolvers/mutations/upsertProfile.ts
+++ src/resolvers/mutations/upsertProfile.ts
@@ -7,10 +7,14 @@ export function upsertProfileResolver(prisma_rw:PrismaClient) {
     return async (parent:any, args:MutationUpsertProfileArgs, context:Context) => {
         const session = await context.verifySession();
         let profile:Profile;
+
         if(args.data.circlesAddress) {
             args.data.circlesAddress = args.data.circlesAddress.toLowerCase();
         }
         if (args.data.id) {
+            if (args.data.id != session.profileId) {
+                throw new Error(`'${session.sessionId}' (profile id: ${session.profileId ?? "<undefined>"}) can not upsert other profile '${args.data.id}'.`);
+            }
             profile = await prisma_rw.profile.update({
                 where: {
                     id: args.data.id
@@ -29,8 +33,8 @@ export function upsertProfileResolver(prisma_rw:PrismaClient) {
                     emailAddress: session.emailAddress
                 }
             });
+            await Session.assignProfile(prisma_rw, session.sessionId, profile.id);
         }
-        await Session.assignProfile(prisma_rw, session.sessionId, profile.id);
         return profile;
     };
 }
