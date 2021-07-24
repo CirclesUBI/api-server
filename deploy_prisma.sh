469d6b6f34e4ad2db454c51053daaf51a8451083
jaensen <daniel@djanz.de>
2021-07-24 22:02:27 +0200

    DB-Model: allow invitations to be "claimed"

--- deploy_prisma.sh
+++ deploy_prisma.sh
@@ -3,4 +3,4 @@ sedArgument="s/REPLACE_ME_WITH_THE_CONNECTION_STRING/${DO_PGSQL_CONNECTIONSTRING
 cp -f schema_template.prisma schema.prisma
 sed -i "${sedArgument}" schema.prisma
 npx prisma --version
-npx prisma db push --preview-feature
+npx prisma db push
