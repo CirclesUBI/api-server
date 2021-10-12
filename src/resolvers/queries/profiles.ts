a60b0d376944fcbfb9a5a6cfad4b8ebfc9b8ac73
Thorsten <letsgo@codeho.net>
2021-10-12 14:19:06 +0200

    Adding displayTimeCircles setting

--- src/resolvers/queries/profiles.ts
+++ src/resolvers/queries/profiles.ts
@@ -1,46 +1,56 @@
-import {Contact, Profile} from "../../types";
-import {Context} from "../../context";
-import {PrismaClient} from "../../api-db/client";
+import { Contact, Profile } from "../../types";
+import { Context } from "../../context";
+import { PrismaClient } from "../../api-db/client";
 import LRU from "lru-cache";
-import {RpcGateway} from "../../rpcGateway";
+import { RpcGateway } from "../../rpcGateway";
 import fetch from "cross-fetch";
-import {contacts} from "./contacts";
+import { contacts } from "./contacts";
 
 export type ProfilesBySafeAddressLookup = {
-  [safeAddress: string]: Profile|null
+  [safeAddress: string]: Profile | null;
 };
 
-const profilesBySafeAddressCache = new LRU<string, Profile|null>({
+const profilesBySafeAddressCache = new LRU<string, Profile | null>({
   max: 20000,
   maxAge: 1000 * 60 * 60 * 24,
   // TODO: Remove updated objects from cache (listen for new transactions)
 });
 
-export function profilesBySafeAddress(prisma:PrismaClient, loadContacts: boolean = false) {
-  return async (parent: any, args: {safeAddresses:string[]}, context: Context) => {
-    let ownProfileId:number|null = null;
-    let ownProfile:Profile|null = null;
+export function profilesBySafeAddress(
+  prisma: PrismaClient,
+  loadContacts: boolean = false
+) {
+  return async (
+    parent: any,
+    args: { safeAddresses: string[] },
+    context: Context
+  ) => {
+    let ownProfileId: number | null = null;
+    let ownProfile: Profile | null = null;
     if (context.sessionId) {
       const session = await context.verifySession();
       ownProfileId = session.profileId;
       if (!ownProfileId)
-        throw new Error(`You must have a complete profile to use this function`);
-      ownProfile = await prisma.profile.findUnique({where: {id: ownProfileId}});
+        throw new Error(
+          `You must have a complete profile to use this function`
+        );
+      ownProfile = await prisma.profile.findUnique({
+        where: { id: ownProfileId },
+      });
       if (!ownProfile)
         throw new Error(`You must have a profile to use this function`);
     } else {
-      throw new Error(`You must have a valid session to use this function`)
+      throw new Error(`You must have a valid session to use this function`);
     }
 
-    const cacheMisses:ProfilesBySafeAddressLookup = {};
+    const cacheMisses: ProfilesBySafeAddressLookup = {};
     const results: Profile[] = [];
 
-    args.safeAddresses.forEach(o => {
+    args.safeAddresses.forEach((o) => {
       const safeAddress = o.toLowerCase();
       const cacheHit = profilesBySafeAddressCache.get(safeAddress);
       if (cacheHit !== undefined) {
-        if (cacheHit != null)
-          results.push(cacheHit);
+        if (cacheHit != null) results.push(cacheHit);
       } else {
         cacheMisses[safeAddress] = null;
       }
@@ -51,12 +61,12 @@ export function profilesBySafeAddress(prisma:PrismaClient, loadContacts: boolean
       const rows = await prisma.profile.findMany({
         where: {
           circlesAddress: {
-            in: Object.keys(cacheMisses)
-          }
-        }
+            in: Object.keys(cacheMisses),
+          },
+        },
       });
 
-      rows.forEach(o => {
+      rows.forEach((o) => {
         results.push(o);
         if (o.circlesAddress) {
           profilesBySafeAddressCache.set(o.circlesAddress, o);
@@ -67,14 +77,18 @@ export function profilesBySafeAddress(prisma:PrismaClient, loadContacts: boolean
       missingProfiles = Object.keys(cacheMisses);
       if (missingProfiles.length > 0) {
         try {
-          const circlesGardenProfiles = await findCirclesGardenProfiles(missingProfiles);
-          circlesGardenProfiles.filter(o => !!o).forEach(o => {
-            results.push(o);
-            if (o.circlesAddress) {
-              profilesBySafeAddressCache.set(o.circlesAddress, o);
-              delete cacheMisses[o.circlesAddress];
-            }
-          });
+          const circlesGardenProfiles = await findCirclesGardenProfiles(
+            missingProfiles
+          );
+          circlesGardenProfiles
+            .filter((o) => !!o)
+            .forEach((o) => {
+              results.push(o);
+              if (o.circlesAddress) {
+                profilesBySafeAddressCache.set(o.circlesAddress, o);
+                delete cacheMisses[o.circlesAddress];
+              }
+            });
         } catch (e) {
           console.error(`Couldn't request profiles from circles.garden:`, e);
         }
@@ -85,26 +99,38 @@ export function profilesBySafeAddress(prisma:PrismaClient, loadContacts: boolean
     if (loadContacts) {
       const contactsResolver = contacts(prisma, false);
       if (ownProfile.circlesAddress) {
-        const _contacts = await contactsResolver(null, {safeAddress: ownProfile.circlesAddress}, context);
-        _contacts.forEach(o => contactsByAddress[o.contactAddress] = o);
+        const _contacts = await contactsResolver(
+          null,
+          { safeAddress: ownProfile.circlesAddress },
+          context
+        );
+        _contacts.forEach((o) => (contactsByAddress[o.contactAddress] = o));
       }
     }
 
-    return results.map(o => {
+    return results.map((o) => {
       return {
         ...o,
         newsletter: ownProfileId == o.id ? o.newsletter : undefined,
-        trustsYou: o.circlesAddress ? contactsByAddress[o.circlesAddress]?.trustsYou : undefined,
-        youTrust: o.circlesAddress ? contactsByAddress[o.circlesAddress]?.youTrust : undefined,
-        lastEvent: o.circlesAddress ? contactsByAddress[o.circlesAddress]?.lastEvent : undefined
-      }
+        displayTimeCircles:
+          ownProfileId == o.id ? o.displayTimeCircles : undefined,
+        trustsYou: o.circlesAddress
+          ? contactsByAddress[o.circlesAddress]?.trustsYou
+          : undefined,
+        youTrust: o.circlesAddress
+          ? contactsByAddress[o.circlesAddress]?.youTrust
+          : undefined,
+        lastEvent: o.circlesAddress
+          ? contactsByAddress[o.circlesAddress]?.lastEvent
+          : undefined,
+      };
     });
   };
 }
 
-export function profilesById(prisma:PrismaClient) {
-  return async (parent: any, args: {ids:number[]}, context: Context) => {
-    let ownProfileId:number|null = null;
+export function profilesById(prisma: PrismaClient) {
+  return async (parent: any, args: { ids: number[] }, context: Context) => {
+    let ownProfileId: number | null = null;
     if (context.sessionId) {
       const session = await context.verifySession();
       ownProfileId = session.profileId;
@@ -112,23 +138,23 @@ export function profilesById(prisma:PrismaClient) {
     const rows = await prisma.profile.findMany({
       where: {
         id: {
-          in: args.ids
-        }
-      }
+          in: args.ids,
+        },
+      },
     });
 
-    return rows.map(o => {
+    return rows.map((o) => {
       return {
         ...o,
         newsletter: ownProfileId == o.id ? o.newsletter : undefined,
-      }
+      };
     });
   };
 }
 
-export function myProfile(prisma:PrismaClient) {
+export function myProfile(prisma: PrismaClient) {
   return async (parent: any, args: any, context: Context) => {
-    let ownProfileId:number|null = null;
+    let ownProfileId: number | null = null;
     if (context.sessionId) {
       const session = await context.verifySession();
       ownProfileId = session.profileId;
@@ -138,8 +164,8 @@ export function myProfile(prisma:PrismaClient) {
     }
     const rows = await prisma.profile.findMany({
       where: {
-        id: ownProfileId
-      }
+        id: ownProfileId,
+      },
     });
     if (rows.length != 1) {
       return null;
@@ -149,101 +175,121 @@ export function myProfile(prisma:PrismaClient) {
 }
 
 type WaitingCirclesGardenProfileRequest = {
-  safeAddress:string,
-  timeoutAt: Date,
-  startedAt: Date|null,
-  finishedAt: Date|null,
+  safeAddress: string;
+  timeoutAt: Date;
+  startedAt: Date | null;
+  finishedAt: Date | null;
   waitingPromises: {
-    resolve: (value: (Profile | PromiseLike<Profile> | null)) => void,
-    reject: (reason?: any) => void
-  }[]
+    resolve: (value: Profile | PromiseLike<Profile> | null) => void;
+    reject: (reason?: any) => void;
+  }[];
 };
 
-let intervalHandle:any = null;
-const requestsBySafeAddress:{[safeAddress:string]:WaitingCirclesGardenProfileRequest} = {};
+let intervalHandle: any = null;
+const requestsBySafeAddress: {
+  [safeAddress: string]: WaitingCirclesGardenProfileRequest;
+} = {};
 
 async function flush() {
-  Object.entries(requestsBySafeAddress).filter(o => o[1].finishedAt).forEach(o => {
-    delete requestsBySafeAddress[o[0]];
-  })
+  Object.entries(requestsBySafeAddress)
+    .filter((o) => o[1].finishedAt)
+    .forEach((o) => {
+      delete requestsBySafeAddress[o[0]];
+    });
   const unprocessed = Object.entries(requestsBySafeAddress)
-    .filter(o => !o[1].startedAt)
-    .map(o => o[1]);
+    .filter((o) => !o[1].startedAt)
+    .map((o) => o[1]);
 
-  if (unprocessed.length == 0){
+  if (unprocessed.length == 0) {
     return;
   }
 
   const now = new Date();
   const batch: WaitingCirclesGardenProfileRequest[] = [];
 
-  unprocessed.filter(o => !o.finishedAt).forEach(request => {
-    if (request.timeoutAt.getTime() < now.getTime()) {
-      request.waitingPromises.forEach(waitingPromise => {
-        const timeoutError = new Error(`The request timed out at ${request.timeoutAt}`);
-        waitingPromise.reject(timeoutError);
-      });
-      return;
-    }
-    if (batch.length > 50) {
-      return;
-    }
-    batch.push(request);
-    request.startedAt = now;
-  });
+  unprocessed
+    .filter((o) => !o.finishedAt)
+    .forEach((request) => {
+      if (request.timeoutAt.getTime() < now.getTime()) {
+        request.waitingPromises.forEach((waitingPromise) => {
+          const timeoutError = new Error(
+            `The request timed out at ${request.timeoutAt}`
+          );
+          waitingPromise.reject(timeoutError);
+        });
+        return;
+      }
+      if (batch.length > 50) {
+        return;
+      }
+      batch.push(request);
+      request.startedAt = now;
+    });
 
   if (batch.length == 0) {
     return;
   }
 
-  const query = batch.reduce((p, c) => p + `address[]=${RpcGateway.get().utils.toChecksumAddress(c.safeAddress)}&`, "");
+  const query = batch.reduce(
+    (p, c) =>
+      p +
+      `address[]=${RpcGateway.get().utils.toChecksumAddress(c.safeAddress)}&`,
+    ""
+  );
   const requestUrl = `https://api.circles.garden/api/users/?${query}`;
 
   console.log(requestUrl);
 
   fetch(requestUrl)
-    .then(result => {
+    .then((result) => {
       return result.json();
     })
-    .then(json => {
-      return json.data.map((o:any) => {
+    .then((json) => {
+      return (
+        json.data.map((o: any) => {
           return <Profile>{
             id: 0,
             firstName: o.username,
             lastName: "",
             circlesAddress: o.safeAddress.toLowerCase(),
-            avatarUrl: o.avatarUrl
-          }
-        })
-        ?? [];
+            avatarUrl: o.avatarUrl,
+          };
+        }) ?? []
+      );
     })
-    .then((profiles:Profile[]) => {
-      const profilesBySafeAddress: {[safeAddress:string]:Profile} = {};
-      profiles.filter(o => o.circlesAddress).forEach(o => profilesBySafeAddress[<string>o.circlesAddress] = o);
+    .then((profiles: Profile[]) => {
+      const profilesBySafeAddress: { [safeAddress: string]: Profile } = {};
+      profiles
+        .filter((o) => o.circlesAddress)
+        .forEach((o) => (profilesBySafeAddress[<string>o.circlesAddress] = o));
 
       const now = new Date();
-      batch.forEach(o => {
+      batch.forEach((o) => {
         o.finishedAt = now;
         const result = profilesBySafeAddress[o.safeAddress];
         if (!result) {
-          console.log(`Couldn't find a circles.garden profile for safe ${o.safeAddress}`);
+          console.log(
+            `Couldn't find a circles.garden profile for safe ${o.safeAddress}`
+          );
           profilesBySafeAddressCache.set(o.safeAddress, null);
-          o.waitingPromises.forEach(p => p.resolve(null));
+          o.waitingPromises.forEach((p) => p.resolve(null));
           return;
         }
-        o.waitingPromises.forEach(p => p.resolve(result));
+        o.waitingPromises.forEach((p) => p.resolve(result));
       });
     })
-    .catch(error => {
+    .catch((error) => {
       const now = new Date();
-      batch.forEach(o => {
+      batch.forEach((o) => {
         o.finishedAt = now;
-        o.waitingPromises.forEach(p => p.reject(error));
+        o.waitingPromises.forEach((p) => p.reject(error));
       });
     });
 }
 
-async function findCirclesGardenProfiles(safeAddresses: string[]) : Promise<Profile[]>  {
+async function findCirclesGardenProfiles(
+  safeAddresses: string[]
+): Promise<Profile[]> {
   if (safeAddresses.length == 0) {
     return [];
   }
@@ -254,43 +300,46 @@ async function findCirclesGardenProfiles(safeAddresses: string[]) : Promise<Prof
     }, 150);
   }
 
-  const results = Promise.all(safeAddresses.map(safeAddress => {
-    let pendingRequest:WaitingCirclesGardenProfileRequest|null = requestsBySafeAddress[safeAddress];
-    if (pendingRequest?.finishedAt) {
-      delete requestsBySafeAddress[safeAddress];
-      pendingRequest = null;
-    }
-
-    let promiseResolver: {
-      resolve: (value: (Profile | PromiseLike<Profile>)) => void,
-      reject: (reason?: any) => void
-    }|null = null;
-
-    const waitPromise = new Promise<Profile>((resolve, reject) => {
-      promiseResolver = {resolve, reject};
-    });
-    if (!promiseResolver) {
-      throw new Error(`!promiseResolver`);
-    }
-
-    if (pendingRequest) {
-      pendingRequest.waitingPromises.push(promiseResolver);
-    } else {
-      pendingRequest = {
-        waitingPromises: [promiseResolver],
-        safeAddress: safeAddress,
-        timeoutAt: new Date(Date.now() + 5000),
-        startedAt: null,
-        finishedAt: null
+  const results = Promise.all(
+    safeAddresses.map((safeAddress) => {
+      let pendingRequest: WaitingCirclesGardenProfileRequest | null =
+        requestsBySafeAddress[safeAddress];
+      if (pendingRequest?.finishedAt) {
+        delete requestsBySafeAddress[safeAddress];
+        pendingRequest = null;
       }
-      requestsBySafeAddress[safeAddress] = pendingRequest;
-    }
 
-    return waitPromise;
-  }));
+      let promiseResolver: {
+        resolve: (value: Profile | PromiseLike<Profile>) => void;
+        reject: (reason?: any) => void;
+      } | null = null;
+
+      const waitPromise = new Promise<Profile>((resolve, reject) => {
+        promiseResolver = { resolve, reject };
+      });
+      if (!promiseResolver) {
+        throw new Error(`!promiseResolver`);
+      }
+
+      if (pendingRequest) {
+        pendingRequest.waitingPromises.push(promiseResolver);
+      } else {
+        pendingRequest = {
+          waitingPromises: [promiseResolver],
+          safeAddress: safeAddress,
+          timeoutAt: new Date(Date.now() + 5000),
+          startedAt: null,
+          finishedAt: null,
+        };
+        requestsBySafeAddress[safeAddress] = pendingRequest;
+      }
+
+      return waitPromise;
+    })
+  );
 
   return results;
-/*
+  /*
   const batchSize = 50;
   const batches = Math.ceil(safeAddresses.length / batchSize);
 
