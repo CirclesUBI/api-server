b294083f21f02dca274b9823cf060e6af6313ea2
Thorsten Schau <letsgo@codeho.net>
2021-12-01 11:29:16 +0100

    remove paid row in invoice

--- src/invoiceGenerator.ts
+++ src/invoiceGenerator.ts
@@ -1,7 +1,7 @@
 import PDFDocument from "pdfkit";
-import {prisma_api_ro} from "./apiDbClient";
+import { prisma_api_ro } from "./apiDbClient";
 import dayjs from "dayjs";
-import {getPool} from "./resolvers/resolvers";
+import { getPool } from "./resolvers/resolvers";
 import AWS from "aws-sdk";
 
 const margin = 30;
@@ -9,55 +9,67 @@ const marginx = 50;
 const lineMargin = 15;
 
 export type PdfInvoiceLine = {
-  amount: number
+  amount: number;
   offer: {
-    title: string,
-    pricePerUnit: number
-  }
+    title: string;
+    pricePerUnit: number;
+  };
 };
 
-export type PdfInvoice =  {
-  invoice_nr: string,
-  invoice_date: string,
-  transferTime: string,
-  transactionHash: string,
+export type PdfInvoice = {
+  invoice_nr: string;
+  invoice_date: string;
+  transferTime: string;
+  transactionHash: string;
   salesTax: {
-    name: string,
-    value: number
-  }[],
-  subtotal: number,
-  timeCirclesTotal: number,
+    name: string;
+    value: number;
+  }[];
+  subtotal: number;
+  timeCirclesTotal: number;
   buyer: {
-    postal_code: string, name: string, address: string, city: string, country: string, safe_address: string
-  },
+    postal_code: string;
+    name: string;
+    address: string;
+    city: string;
+    country: string;
+    safe_address: string;
+  };
   seller: {
-    postal_code: string, name: string, address: string, city: string, country: string, safe_address: string
-  },
-  items: PdfInvoiceLine[]
+    postal_code: string;
+    name: string;
+    address: string;
+    city: string;
+    country: string;
+    safe_address: string;
+  };
+  items: PdfInvoiceLine[];
 };
 
 let top = margin;
 let path = ""; // PATH TO PDF FILE.
-let invoice:PdfInvoice;
+let invoice: PdfInvoice;
 
 export async function createPdfForInvoice(invoiceId: number, path: string) {
   const invoiceFromDb = await prisma_api_ro.invoice.findUnique({
     where: {
-      id: invoiceId
+      id: invoiceId,
     },
     include: {
       customerProfile: true,
       sellerProfile: true,
       lines: {
         include: {
-          product: true
-        }
-      }
-    }
+          product: true,
+        },
+      },
+    },
   });
 
   if (!invoiceFromDb) {
-    throw new Error(`Couldn't find the invoice with id ${invoiceId} for pdf generation.`)
+    throw new Error(
+      `Couldn't find the invoice with id ${invoiceId} for pdf generation.`
+    );
   }
 
   const buyer = invoiceFromDb.customerProfile;
@@ -69,59 +81,71 @@ export async function createPdfForInvoice(invoiceId: number, path: string) {
       `select *
         from transaction_2
         where hash = $1`,
-      [invoiceFromDb.paymentTransactionHash])
+      [invoiceFromDb.paymentTransactionHash]
+    );
     if (transactionsResult.rows.length > 0) {
       paymentTransaction = transactionsResult.rows[0];
     }
   }
 
   if (!buyer.circlesAddress || !seller.circlesAddress) {
-    throw new Error(`Couldn't create a pdf for invoice ${invoiceFromDb.invoiceNo} because the seller or buyer has no circlesAddress.`);
+    throw new Error(
+      `Couldn't create a pdf for invoice ${invoiceFromDb.invoiceNo} because the seller or buyer has no circlesAddress.`
+    );
   }
 
-  const items = invoiceFromDb.lines.map(o => {
+  const items = invoiceFromDb.lines.map((o) => {
     return <PdfInvoiceLine>{
       offer: {
         pricePerUnit: parseFloat(o.product.pricePerUnit),
-        title: o.product.title
+        title: o.product.title,
       },
-      amount: o.amount
-    }
+      amount: o.amount,
+    };
   });
-  const total = items.reduce((p,c) => p + c.amount * c.offer.pricePerUnit, 0);
+  const total = items.reduce((p, c) => p + c.amount * c.offer.pricePerUnit, 0);
 
-  createInvoice({
-    buyer: {
-      safe_address: buyer.circlesAddress,
-      name: displayableName(buyer.firstName, buyer.lastName),
-      address: "",
-      city: "",
-      country: "",
-      postal_code: ""
+  createInvoice(
+    {
+      buyer: {
+        safe_address: buyer.circlesAddress,
+        name: displayableName(buyer.firstName, buyer.lastName),
+        address: "",
+        city: "",
+        country: "",
+        postal_code: "",
+      },
+      seller: {
+        name: displayableName(seller.firstName, seller.lastName),
+        address: "Reifenstuehlstr. 9",
+        city: "München",
+        country: "Deutschland",
+        postal_code: "80469",
+        safe_address: seller.circlesAddress,
+      },
+      invoice_date: dayjs(invoiceFromDb.createdAt).format(
+        "YYYY-MM-DD HH:mm:ss"
+      ),
+      invoice_nr: invoiceFromDb.invoiceNo,
+      transactionHash: paymentTransaction?.hash ?? "",
+      transferTime: paymentTransaction
+        ? dayjs(paymentTransaction.timestamp).format("YYYY-MM-DD HH:mm:ss")
+        : "",
+      items: items,
+      subtotal: total,
+      salesTax: [
+        {
+          name: "",
+          value: 19,
+        },
+      ],
+      timeCirclesTotal: 0,
     },
-    seller: {
-      name: displayableName(seller.firstName, seller.lastName),
-      address: "Reifenstuehlstr. 9",
-      city: "München",
-      country: "Deutschland",
-      postal_code: "80469",
-      safe_address: seller.circlesAddress
-    },
-    invoice_date: dayjs(invoiceFromDb.createdAt).format("YYYY-MM-DD HH:mm:ss"),
-    invoice_nr: invoiceFromDb.invoiceNo,
-    transactionHash: paymentTransaction?.hash ?? "",
-    transferTime: paymentTransaction ? dayjs(paymentTransaction.timestamp).format("YYYY-MM-DD HH:mm:ss") : "",
-    items: items,
-    subtotal: total,
-    salesTax: [{
-      name: "",
-      value: 19
-    }],
-    timeCirclesTotal: 0
-  }, path);
+    path
+  );
 }
 
-export async function createInvoice(invoice: PdfInvoice, path:string) {
+export async function createInvoice(invoice: PdfInvoice, path: string) {
   let doc = new PDFDocument({
     size: "A4",
     margins: { top: margin, left: margin, bottom: 10, right: 50 },
@@ -139,29 +163,33 @@ export async function createInvoice(invoice: PdfInvoice, path:string) {
   doc.end();
 
   if (!process.env.DIGITALOCEAN_SPACES_ENDPOINT)
-    throw new Error(`Missing configuration: process.env.DIGITALOCEAN_SPACES_ENDPOINT`);
+    throw new Error(
+      `Missing configuration: process.env.DIGITALOCEAN_SPACES_ENDPOINT`
+    );
 
-  const spacesEndpoint = new AWS.Endpoint(process.env.DIGITALOCEAN_SPACES_ENDPOINT);
+  const spacesEndpoint = new AWS.Endpoint(
+    process.env.DIGITALOCEAN_SPACES_ENDPOINT
+  );
   const s3 = new AWS.S3({
     endpoint: spacesEndpoint,
     accessKeyId: process.env.DIGITALOCEAN_SPACES_KEY,
-    secretAccessKey: process.env.DIGITALOCEAN_SPACES_SECRET
+    secretAccessKey: process.env.DIGITALOCEAN_SPACES_SECRET,
   });
 
-  var params:{
-    Bucket: string,
-    Body?: any,
-    Key: string,
-    ACL: string
+  var params: {
+    Bucket: string;
+    Body?: any;
+    Key: string;
+    ACL: string;
   } = {
     Bucket: "circlesland-invoices",
     Key: `${invoice.seller.safe_address}/${invoice.invoice_nr}.pdf`,
-    ACL: 'private'
+    ACL: "private",
   };
 
-  let pdfBytes:any[] = [];
-  doc.on('readable', _ => {
-    while(true) {
+  let pdfBytes: any[] = [];
+  doc.on("readable", (_) => {
+    while (true) {
       let buffer = doc.read();
       if (!buffer) {
         break;
@@ -169,14 +197,18 @@ export async function createInvoice(invoice: PdfInvoice, path:string) {
       pdfBytes.push(buffer);
     }
   });
-  doc.on('end', async _ => {
+  doc.on("end", async (_) => {
     const buffer = Buffer.concat(pdfBytes);
     params.Body = buffer;
     await s3.putObject(params).promise();
   });
 }
 
-function newPageCheck(doc:(typeof PDFDocument), invoice: any, itemsHeader = false) {
+function newPageCheck(
+  doc: typeof PDFDocument,
+  invoice: any,
+  itemsHeader = false
+) {
   if (top > 730) {
     generateFooter(doc, invoice);
     doc.addPage();
@@ -193,7 +225,11 @@ function newPageCheck(doc:(typeof PDFDocument), invoice: any, itemsHeader = fals
   }
 }
 
-function generateHeader(doc:(typeof PDFDocument), sellerLogo: string, invoice: { invoice_nr: string, invoice_date: string}) {
+function generateHeader(
+  doc: typeof PDFDocument,
+  sellerLogo: string,
+  invoice: { invoice_nr: string; invoice_date: string }
+) {
   let sectionTop = top;
   doc
     .rect(0, 0, doc.page.width, 120)
@@ -221,10 +257,27 @@ function generateHeader(doc:(typeof PDFDocument), sellerLogo: string, invoice: {
     .moveDown();
 }
 
-function generateCustomerInformation(doc:(typeof PDFDocument), invoice: {
-  buyer: {name: string, address:string, postal_code: string, city: string, country: string, safe_address: string},
-  seller: {name: string, address:string, postal_code: string, city: string, country: string, safe_address: string}
-}) {
+function generateCustomerInformation(
+  doc: typeof PDFDocument,
+  invoice: {
+    buyer: {
+      name: string;
+      address: string;
+      postal_code: string;
+      city: string;
+      country: string;
+      safe_address: string;
+    };
+    seller: {
+      name: string;
+      address: string;
+      postal_code: string;
+      city: string;
+      country: string;
+      safe_address: string;
+    };
+  }
+) {
   let sectionTop = (top += 70);
 
   doc
@@ -278,11 +331,14 @@ function generateCustomerInformation(doc:(typeof PDFDocument), invoice: {
     .moveDown();
 }
 
-function generateSubtotalTable(doc:(typeof PDFDocument), invoice: {
-  salesTax: { name: string, value: number }[],
-  subtotal: number,
-  timeCirclesTotal: number
-}) {
+function generateSubtotalTable(
+  doc: typeof PDFDocument,
+  invoice: {
+    salesTax: { name: string; value: number }[];
+    subtotal: number;
+    timeCirclesTotal: number;
+  }
+) {
   if (top > 680) {
     doc.addPage();
     top = margin;
@@ -346,19 +402,19 @@ function generateSubtotalTable(doc:(typeof PDFDocument), invoice: {
   //   top = margin;
   // }
 
-  doc
-    .fontSize(10)
-    .font("Helvetica")
-    .fillColor("#333333")
+  // doc
+  //   .fontSize(10)
+  //   .font("Helvetica")
+  //   .fillColor("#333333")
 
-    .text("Paid (Time Circles)", 300, (top += 20), {
-      width: 100,
-      align: "left",
-    })
-    .text(`-${invoice.timeCirclesTotal} c`, 0, top, {
-      align: "right",
-    })
-    .moveDown();
+  //   .text("Paid (Time Circles)", 300, (top += 20), {
+  //     width: 100,
+  //     align: "left",
+  //   })
+  //   .text(`-${invoice.timeCirclesTotal} c`, 0, top, {
+  //     align: "right",
+  //   })
+  //   .moveDown();
 
   newPageCheck(doc, invoice);
 
@@ -382,7 +438,10 @@ function generateSubtotalTable(doc:(typeof PDFDocument), invoice: {
   newPageCheck(doc, invoice);
 }
 
-function generateTransactionMetaData(doc:(typeof PDFDocument), invoice: {transferTime: string, transactionHash: string}) {
+function generateTransactionMetaData(
+  doc: typeof PDFDocument,
+  invoice: { transferTime: string; transactionHash: string }
+) {
   top += 40;
   newPageCheck(doc, invoice);
 
@@ -407,12 +466,15 @@ function generateTransactionMetaData(doc:(typeof PDFDocument), invoice: {transfe
     .text(invoice.transactionHash, marginx, (top += lineMargin));
 }
 
-function generateInvoiceTable(doc:(typeof PDFDocument), invoice: {
-  items: {
-    amount: number
-    offer: {title: string, pricePerUnit: number}
-  }[]
-}) {
+function generateInvoiceTable(
+  doc: typeof PDFDocument,
+  invoice: {
+    items: {
+      amount: number;
+      offer: { title: string; pricePerUnit: number };
+    }[];
+  }
+) {
   let i;
   top += 40;
   doc
@@ -449,7 +511,7 @@ function generateInvoiceTable(doc:(typeof PDFDocument), invoice: {
   }
 }
 
-function generateFooter(doc:(typeof PDFDocument), invoice: any) {
+function generateFooter(doc: typeof PDFDocument, invoice: any) {
   doc
     .font("Helvetica")
     .fontSize(8)
@@ -480,12 +542,12 @@ function generateFooter(doc:(typeof PDFDocument), invoice: any) {
 }
 
 function generateTableRow(
-  doc:(typeof PDFDocument),
-  y:number,
-  item:string,
-  unitCost:string,
-  quantity:string,
-  lineTotal:string
+  doc: typeof PDFDocument,
+  y: number,
+  item: string,
+  unitCost: string,
+  quantity: string,
+  lineTotal: string
 ) {
   doc
     .fontSize(10)
@@ -498,15 +560,15 @@ function generateTableRow(
     .text(lineTotal, 0, y, { align: "right" });
 }
 
-function generateHr(doc:(typeof PDFDocument), y: number) {
+function generateHr(doc: typeof PDFDocument, y: number) {
   doc.strokeColor("#aaaaaa").lineWidth(1).moveTo(50, y).lineTo(550, y).stroke();
 }
 
-function formatCurrency(amount:number) {
+function formatCurrency(amount: number) {
   return amount + " €";
 }
 
-function formatDate(date:Date) {
+function formatDate(date: Date) {
   const day = date.getDate();
   const month = date.getMonth() + 1;
   const year = date.getFullYear();
@@ -514,6 +576,6 @@ function formatDate(date:Date) {
   return year + "/" + month + "/" + day;
 }
 
-export function displayableName(firstName: string, lastName?: string|null) {
+export function displayableName(firstName: string, lastName?: string | null) {
   return `${firstName} ${lastName ? lastName : ""}`;
 }
