2a73aef7d75c0d437d2e7954c47a3652951657aa
Thorsten <letsgo@codeho.net>
2021-12-16 18:20:29 +0100

    add condition to profile counter

--- src/resolvers/queries/profilesCount.ts
+++ src/resolvers/queries/profilesCount.ts
@@ -2,6 +2,11 @@ import { PrismaClient } from "../../api-db/client";
 
 export function profilesCount(prisma: PrismaClient) {
   return async () => {
-    return await prisma.profile.count();
+    return await prisma.profile.count({
+      where: {
+        firstName: { not: "" },
+        circlesAddress: { not: null },
+      },
+    });
   };
 }
