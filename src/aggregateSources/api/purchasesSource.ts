9c88a2acc33c81e45d28370b630979043699a8fd
Thorsten Schau <letsgo@codeho.net>
2022-02-16 11:42:26 +0100

    changed purchaseCode filter from every to some

--- src/aggregateSources/api/purchasesSource.ts
+++ src/aggregateSources/api/purchasesSource.ts
@@ -1,37 +1,43 @@
-import {AggregateSource} from "../aggregateSource";
+import { AggregateSource } from "../aggregateSource";
 import {
   AggregateType,
   Maybe,
   ProfileAggregate,
   ProfileAggregateFilter,
-  Purchases
+  Purchases,
 } from "../../types";
-import {Environment} from "../../environment";
+import { Environment } from "../../environment";
 
 export class PurchasesSource implements AggregateSource {
-  async getAggregate(forSafeAddress: string, filter?: Maybe<ProfileAggregateFilter>): Promise<ProfileAggregate[]> {
-
-    const idFilter = filter?.purchases?.purchaseIds ? {
-      id: {
-        in: filter?.purchases?.purchaseIds ?? []
-      }
-    } : {};
-
-    const pickupCodeFilter = filter?.purchases?.pickupCode ? {
-      invoices: {
-        every: {
-          pickupCode: filter?.purchases?.pickupCode
+  async getAggregate(
+    forSafeAddress: string,
+    filter?: Maybe<ProfileAggregateFilter>
+  ): Promise<ProfileAggregate[]> {
+    const idFilter = filter?.purchases?.purchaseIds
+      ? {
+          id: {
+            in: filter?.purchases?.purchaseIds ?? [],
+          },
         }
-      }
-    } : {};
+      : {};
+
+    const pickupCodeFilter = filter?.purchases?.pickupCode
+      ? {
+          invoices: {
+            some: {
+              pickupCode: filter?.purchases?.pickupCode,
+            },
+          },
+        }
+      : {};
 
     const purchasesResult = await Environment.readonlyApiDb.purchase.findMany({
       where: {
         createdBy: {
-          circlesAddress: forSafeAddress
+          circlesAddress: forSafeAddress,
         },
         ...idFilter,
-        ...pickupCodeFilter
+        ...pickupCodeFilter,
       },
       include: {
         createdBy: true,
@@ -39,43 +45,56 @@ export class PurchasesSource implements AggregateSource {
           include: {
             product: {
               include: {
-                createdBy: true
-              }
-            }
-          }
-        }
+                createdBy: true,
+              },
+            },
+          },
+        },
       },
       orderBy: {
-        createdAt: "desc"
-      }
+        createdAt: "desc",
+      },
     });
 
-    const lastUpdatedAt = new Date(purchasesResult.reduce((p,c) => Math.max(p, new Date(c.createdAt).getTime()), 0));
+    const lastUpdatedAt = new Date(
+      purchasesResult.reduce(
+        (p, c) => Math.max(p, new Date(c.createdAt).getTime()),
+        0
+      )
+    );
 
-    return [<ProfileAggregate>{
-      type: AggregateType.Purchases,
-      safe_address: forSafeAddress,
-      lastUpdatedAt: lastUpdatedAt.toJSON(),
-      payload: <Purchases> {
-        __typename: "Purchases",
+    return [
+      <ProfileAggregate>{
+        type: AggregateType.Purchases,
+        safe_address: forSafeAddress,
         lastUpdatedAt: lastUpdatedAt.toJSON(),
-        purchases: <any>purchasesResult.map(o => {
-          const total = o.lines.reduce((p,c) => p + c.amount * parseFloat(c.product.pricePerUnit), 0).toString();
-          return <any>{
-            ...o,
-            total: total,
-            lines: o.lines.map(p => {
-              (<any>p.product).createdByAddress = p.product.createdBy.circlesAddress;
-              return {
-                ...p,
-                offer: p.product
-              }
-            }),
-            createdAt: o.createdAt.toJSON(),
-            createdByAddress: o.createdBy.circlesAddress,
-          }
-        })
-      }
-    }]
+        payload: <Purchases>{
+          __typename: "Purchases",
+          lastUpdatedAt: lastUpdatedAt.toJSON(),
+          purchases: <any>purchasesResult.map((o) => {
+            const total = o.lines
+              .reduce(
+                (p, c) => p + c.amount * parseFloat(c.product.pricePerUnit),
+                0
+              )
+              .toString();
+            return <any>{
+              ...o,
+              total: total,
+              lines: o.lines.map((p) => {
+                (<any>p.product).createdByAddress =
+                  p.product.createdBy.circlesAddress;
+                return {
+                  ...p,
+                  offer: p.product,
+                };
+              }),
+              createdAt: o.createdAt.toJSON(),
+              createdByAddress: o.createdBy.circlesAddress,
+            };
+          }),
+        },
+      },
+    ];
   }
 }
--- src/aggregateSources/api/salesSource.ts
+++ src/aggregateSources/api/salesSource.ts
@@ -1,26 +1,33 @@
-import {AggregateSource} from "../aggregateSource";
+import { AggregateSource } from "../aggregateSource";
 import {
   AggregateType,
   Maybe,
   ProfileAggregate,
-  ProfileAggregateFilter, Sales
+  ProfileAggregateFilter,
+  Sales,
 } from "../../types";
-import {Environment} from "../../environment";
+import { Environment } from "../../environment";
 
 export class SalesSource implements AggregateSource {
-  async getAggregate(forSafeAddress: string, filter?: Maybe<ProfileAggregateFilter>): Promise<ProfileAggregate[]> {
-
-    const idFilter = filter?.sales?.salesIds ? {
-      id: {
-        in: filter?.sales?.salesIds ?? []
-      }
-    } : {};
-
-    const pickupCodeFilter = filter?.sales?.pickupCode ? {
-        every: {
-          pickupCode: filter?.sales?.pickupCode
+  async getAggregate(
+    forSafeAddress: string,
+    filter?: Maybe<ProfileAggregateFilter>
+  ): Promise<ProfileAggregate[]> {
+    const idFilter = filter?.sales?.salesIds
+      ? {
+          id: {
+            in: filter?.sales?.salesIds ?? [],
+          },
         }
-    } : {};
+      : {};
+
+    const pickupCodeFilter = filter?.sales?.pickupCode
+      ? {
+          some: {
+            pickupCode: filter?.sales?.pickupCode,
+          },
+        }
+      : {};
 
     const salesResult = await Environment.readonlyApiDb.purchase.findMany({
       where: {
@@ -28,29 +35,29 @@ export class SalesSource implements AggregateSource {
         invoices: {
           some: {
             sellerProfile: {
-              circlesAddress: forSafeAddress
-            }
+              circlesAddress: forSafeAddress,
+            },
           },
-          ...pickupCodeFilter
-        }
+          ...pickupCodeFilter,
+        },
       },
       orderBy: {
-        createdAt: "desc"
+        createdAt: "desc",
       },
       include: {
         invoices: {
           include: {
             sellerProfile: {
               select: {
-                circlesAddress: true
-              }
+                circlesAddress: true,
+              },
             },
             customerProfile: {
               select: {
-                circlesAddress: true
-              }
-            }
-          }
+                circlesAddress: true,
+              },
+            },
+          },
         },
         lines: {
           include: {
@@ -58,51 +65,64 @@ export class SalesSource implements AggregateSource {
               include: {
                 createdBy: {
                   select: {
-                    circlesAddress: true
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
+                    circlesAddress: true,
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
     });
 
-    const lastUpdatedAt = new Date(salesResult.reduce((p,c) => Math.max(p, new Date(c.createdAt).getTime()), 0));
+    const lastUpdatedAt = new Date(
+      salesResult.reduce(
+        (p, c) => Math.max(p, new Date(c.createdAt).getTime()),
+        0
+      )
+    );
 
-    return [<ProfileAggregate>{
-      type: AggregateType.Sales,
-      safe_address: forSafeAddress,
-      lastUpdatedAt: lastUpdatedAt.toJSON(),
-      payload: <Sales> {
-        __typename: "Sales",
+    return [
+      <ProfileAggregate>{
+        type: AggregateType.Sales,
+        safe_address: forSafeAddress,
         lastUpdatedAt: lastUpdatedAt.toJSON(),
-        sales: <any>salesResult.map(o => {
-          const total = o.lines.reduce((p,c) => p + c.amount * parseFloat(c.product.pricePerUnit), 0).toString();
-          return <any>{
-            ...o,
-            sellerAddress: o.invoices[0].sellerProfile.circlesAddress,
-            buyerAddress: o.invoices[0].customerProfile.circlesAddress,
-            total: total,
-            lines: o.lines.map(p => {
-              (<any>p.product).createdByAddress = p.product.createdBy.circlesAddress;
-              return {
-                ...p,
-                offer: p.product
-              }
-            }),
-            invoices: o.invoices.map(i => {
-              return {
-                ...i,
-                sellerAddress: o.invoices[0].sellerProfile.circlesAddress,
-                buyerAddress: o.invoices[0].customerProfile.circlesAddress
-              }
-            }),
-            createdAt: o.createdAt.toJSON(),
-            createdByAddress: forSafeAddress,
-          }
-        })
-      }
-    }]
+        payload: <Sales>{
+          __typename: "Sales",
+          lastUpdatedAt: lastUpdatedAt.toJSON(),
+          sales: <any>salesResult.map((o) => {
+            const total = o.lines
+              .reduce(
+                (p, c) => p + c.amount * parseFloat(c.product.pricePerUnit),
+                0
+              )
+              .toString();
+            return <any>{
+              ...o,
+              sellerAddress: o.invoices[0].sellerProfile.circlesAddress,
+              buyerAddress: o.invoices[0].customerProfile.circlesAddress,
+              total: total,
+              lines: o.lines.map((p) => {
+                (<any>p.product).createdByAddress =
+                  p.product.createdBy.circlesAddress;
+                return {
+                  ...p,
+                  offer: p.product,
+                };
+              }),
+              invoices: o.invoices.map((i) => {
+                return {
+                  ...i,
+                  sellerAddress: o.invoices[0].sellerProfile.circlesAddress,
+                  buyerAddress: o.invoices[0].customerProfile.circlesAddress,
+                };
+              }),
+              createdAt: o.createdAt.toJSON(),
+              createdByAddress: forSafeAddress,
+            };
+          }),
+        },
+      },
+    ];
   }
 }
