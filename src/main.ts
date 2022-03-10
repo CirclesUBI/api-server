ee120549bc63d98c6be6ba6d940a4b6a2ad7debc
jaensen <daniel@circles.name>
2022-03-10 03:57:33 +0100

    send a Purchased and Sale event

--- src/main.ts
+++ src/main.ts
@@ -24,136 +24,137 @@ import {Echo} from "./jobs/descriptions/echo";
 import {InviteCodeFromExternalTrigger} from "./jobs/descriptions/onboarding/inviteCodeFromExternalTrigger";
 
 const {
-  ApolloServerPluginLandingPageGraphQLPlayground,
+    ApolloServerPluginLandingPageGraphQLPlayground,
 } = require("apollo-server-core");
 
 const corsOrigins = Environment.corsOrigins.split(";").map((o) => o.trim());
 
 export class Main {
-  async run() {
-    RpcGateway.setup(Environment.rpcGatewayUrl, Environment.fixedGasPrice);
+    async run() {
+        RpcGateway.setup(Environment.rpcGatewayUrl, Environment.fixedGasPrice);
 
-    if (Environment.delayStart) {
-      console.log(
-        `Delaying the start for ${Environment.delayStart} seconds ...`
-      );
-      await new Promise((r) => {
-        setTimeout(r, Environment.delayStart * 1000);
-      });
+        if (Environment.delayStart) {
+            console.log(
+              `Delaying the start for ${Environment.delayStart} seconds ...`
+            );
+            await new Promise((r) => {
+                setTimeout(r, Environment.delayStart * 1000);
+            });
+        }
+
+        console.log(`Starting instance '${Environment.instanceId} ...'`);
+        console.log("======== Checking configuration ======== ");
+        await Environment.validateAndSummarize();
+        console.log("================ DONE ================== ");
+        console.log("");
+
+        const app = express();
+        let corsOptions = {
+            origin: corsOrigins,
+            credentials: true,
+        };
+        app.use(cors(corsOptions));
+        app.use(express.json({limit: "10mb"}));
+        app.use(
+          express.urlencoded({
+              limit: "10mb",
+              extended: true
+          })
+        );
+
+        app.post("/upload", cors(corsOptions), uploadPostHandler);
+        app.get("/trigger", cors(corsOptions), triggerGetHandler);
+
+        const httpServer = createServer(app);
+        const schema = makeExecutableSchema({
+            typeDefs: graphqlImport.processImport("../src/server-schema.graphql"),
+            resolvers,
+        });
+
+        let subscriptionServer: SubscriptionServer|null = null;
+
+        const server = new ApolloServer({
+            schema,
+            context: Context.create,
+            plugins: [
+                {
+                    async serverWillStart() {
+                        return {
+                            async drainServer() {
+                                subscriptionServer?.close();
+                            },
+                        };
+                    },
+                },
+                ApolloServerPluginLandingPageGraphQLPlayground(),
+                new GqlLogger(),
+            ],
+        });
+
+        subscriptionServer = await gqlSubscriptionServer(schema, httpServer, server.graphqlPath);
+
+        await server.start();
+        const serverMiddleware = server.getMiddleware({
+            path: "/",
+            cors: {
+                origin: corsOrigins,
+                credentials: true,
+            },
+        });
+
+        app.use(serverMiddleware);
+        app.use(function (err: any) {
+            console.error(err.stack);
+        });
+
+        const indexerEventProcessor = new IndexerEvents(
+          Environment.blockchainIndexerUrl,
+          2500,
+          [
+              new PaymentProcessor(),
+              new AppNotificationProcessor()
+          ]
+        );
+
+        indexerEventProcessor.run();
+
+        const jobQueue = new JobQueue("jobQueue");
+        const jobTopics:JobType[] = [
+            "broadcastChatMessage",
+            "sendCrcReceivedEmail",
+            "sendCrcTrustChangedEmail",
+            "sendOrderConfirmationEmail",
+            "invoicePayed",
+            "verifyEmailAddress",
+            "sendVerifyEmailAddressEmail",
+            "inviteCodeFromExternalTrigger",
+            "echo",
+            "broadcastPurchased"
+        ];
+
+        jobQueue.consume(jobTopics, jobSink, false)
+          .then(() => {
+              console.info("JobQueue stopped.");
+          }).catch(e => {
+            console.error("JobQueue crashed.", e);
+        });
+
+        // TODO: Add follow trust job handling
+
+        // await JobQueue.produce([new InviteCodeFromExternalTrigger("event_1", "http://localhost:5000", "0xde374ece6fa50e781e81aac78e811b33d16912c7")]);
+        /*
+            this.listenForDbEvents("follow_trust").catch((e) => {
+              console.error(`The notifyConnection for 'follow_trust' events died:`, e);
+            });
+        */
+
+        const PORT = 8989;
+        httpServer.listen(PORT, () =>
+          console.log(`Server is now running on http://localhost:${PORT}/graphql`)
+        );
     }
-
-    console.log(`Starting instance '${Environment.instanceId} ...'`);
-    console.log("======== Checking configuration ======== ");
-    await Environment.validateAndSummarize();
-    console.log("================ DONE ================== ");
-    console.log("");
-
-    const app = express();
-    let corsOptions = {
-      origin: corsOrigins,
-      credentials: true,
-    };
-    app.use(cors(corsOptions));
-    app.use(express.json({limit: "10mb"}));
-    app.use(
-      express.urlencoded({
-        limit: "10mb",
-        extended: true
-      })
-    );
-
-    app.post("/upload", cors(corsOptions), uploadPostHandler);
-    app.get("/trigger", cors(corsOptions), triggerGetHandler);
-
-    const httpServer = createServer(app);
-    const schema = makeExecutableSchema({
-      typeDefs: graphqlImport.processImport("../src/server-schema.graphql"),
-      resolvers,
-    });
-
-    let subscriptionServer: SubscriptionServer|null = null;
-
-    const server = new ApolloServer({
-      schema,
-      context: Context.create,
-      plugins: [
-        {
-          async serverWillStart() {
-            return {
-              async drainServer() {
-                subscriptionServer?.close();
-              },
-            };
-          },
-        },
-        ApolloServerPluginLandingPageGraphQLPlayground(),
-        new GqlLogger(),
-      ],
-    });
-
-    subscriptionServer = await gqlSubscriptionServer(schema, httpServer, server.graphqlPath);
-
-    await server.start();
-    const serverMiddleware = server.getMiddleware({
-      path: "/",
-      cors: {
-        origin: corsOrigins,
-        credentials: true,
-      },
-    });
-
-    app.use(serverMiddleware);
-    app.use(function (err: any) {
-      console.error(err.stack);
-    });
-
-    const indexerEventProcessor = new IndexerEvents(
-      Environment.blockchainIndexerUrl,
-      2500,
-      [
-        new PaymentProcessor(),
-        new AppNotificationProcessor()
-      ]
-    );
-
-    indexerEventProcessor.run();
-
-    const jobQueue = new JobQueue("jobQueue");
-    const jobTopics:JobType[] = [
-      "broadcastChatMessage",
-      "sendCrcReceivedEmail",
-      "sendCrcTrustChangedEmail",
-      "sendOrderConfirmationEmail",
-      "invoicePayed",
-      "verifyEmailAddress",
-      "sendVerifyEmailAddressEmail",
-      "inviteCodeFromExternalTrigger",
-      "echo"
-    ];
-
-    jobQueue.consume(jobTopics, jobSink, false)
-      .then(() => {
-        console.info("JobQueue stopped.");
-      }).catch(e => {
-        console.error("JobQueue crashed.", e);
-      });
-
-    // TODO: Add follow trust job handling
-
-    // await JobQueue.produce([new InviteCodeFromExternalTrigger("event_1", "http://localhost:5000", "0xde374ece6fa50e781e81aac78e811b33d16912c7")]);
-/*
-    this.listenForDbEvents("follow_trust").catch((e) => {
-      console.error(`The notifyConnection for 'follow_trust' events died:`, e);
-    });
-*/
-
-    const PORT = 8989;
-    httpServer.listen(PORT, () =>
-      console.log(`Server is now running on http://localhost:${PORT}/graphql`)
-    );
-  }
 }
 
 new Main().run().then(() => console.log("Started")).then(async () => {
-  // await ninetyDaysLater()
+    // await ninetyDaysLater()
 });
