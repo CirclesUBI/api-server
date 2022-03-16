5be0ad8e79269be342d3b9a61d4b90481f309e93
jaensen <daniel@circles.name>
2022-03-16 16:22:40 +0100

    log trigger executions

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,9 +1,11 @@
 import {Environment} from "../environment";
 
-export function log(prefix: string, identifiers:string, message: string) {
-    console.log(
-        `${prefix}[${new Date().toJSON()}] [${Environment.instanceId}] ${identifiers}: ${message}`
-    );
+export function log(prefix: string, identifiers:string, message: string, obj?:any) {
+    const msg = `${prefix}[${new Date().toJSON()}] [${Environment.instanceId}] ${identifiers}: ${message}`;
+    if (!obj)
+        console.log(msg);
+    else
+        console.log(msg, obj)
 }
 
 export function logErr(prefix: string, identifiers:string, message: string) {
