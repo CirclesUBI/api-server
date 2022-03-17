e599f8de624e762ca7373636dea735fb838acdfe
jaensen <daniel@circles.name>
2022-03-17 17:11:26 +0100

    send the Sale-event to all orga members

--- src/jobs/worker/market/broadcastPurchasedWorker.ts
+++ src/jobs/worker/market/broadcastPurchasedWorker.ts
@@ -1,16 +1,16 @@
 import {ApiPubSub} from "../../../utils/pubsub";
 import {RpcGateway} from "../../../circles/rpcGateway";
-import {BroadcastChatMessage} from "../../descriptions/chat/broadcastChatMessage";
 import {JobWorker, JobWorkerConfiguration} from "../jobWorker";
 import {BroadcastPurchased} from "../../descriptions/market/broadcastPurchased";
 import {EventType} from "../../../types";
+import {Environment} from "../../../environment";
 
 export class BroadcastPurchasedWorker extends JobWorker<BroadcastPurchased> {
   name(): string {
     return "BroadcastChatMessageWorker";
   }
 
-  constructor(configuration?:JobWorkerConfiguration) {
+  constructor(configuration?: JobWorkerConfiguration) {
     super(configuration);
   }
 
@@ -29,14 +29,41 @@ export class BroadcastPurchasedWorker extends JobWorker<BroadcastPurchased> {
         itemId: job.purchaseId
       },
     });
-    await ApiPubSub.instance.pubSub.publish(`events_${job.to}`, {
-      events: {
-        type: EventType.SaleEvent,
-        from: job.from,
-        to: job.to,
-        itemId: job.purchaseId
-      },
+
+    const invoice = await Environment.readWriteApiDb.invoice.findMany({
+      where: {
+        purchaseId: job.purchaseId,
+        sellerProfile: {
+          circlesAddress: job.to
+        }
+      }
     });
+
+    if (invoice.length > 0) {
+      const saleEvent = {
+        events: {
+          type: EventType.SaleEvent,
+          from: job.from,
+          to: job.to,
+          itemId: invoice[0].id
+        },
+      };
+
+      const maybeAnOrgaResult = await Environment.readonlyApiDb.profile.findFirst({
+        where: {
+          circlesAddress: job.to,
+          type: "ORGANISATION"
+        },
+        include: {
+          members: true
+        }
+      });
+      if (maybeAnOrgaResult) {
+        await Promise.all(maybeAnOrgaResult.members?.map(async m => {
+          await ApiPubSub.instance.pubSub.publish(`events_${m.memberAddress}`, saleEvent);
+        }));
+      }
+    }
 
     return undefined;
   }
