7583d603728e2446425849345fd946bc52b133fc
Thorsten Schau <letsgo@codeho.net>
2022-03-18 11:03:06 +0100

    change to only firstname

--- src/jobs/worker/emailNotifications/sendCrcTrustChangedEmailWorker.ts
+++ src/jobs/worker/emailNotifications/sendCrcTrustChangedEmailWorker.ts
@@ -1,16 +1,16 @@
-import {JobWorker, JobWorkerConfiguration} from "../jobWorker";
-import {SendCrcTrustChangedEmail} from "../../descriptions/emailNotifications/sendCrcTrustChangedEmail";
-import {Mailer} from "../../../mailer/mailer";
-import {ProfileLoader} from "../../../querySources/profileLoader";
-import {Environment} from "../../../environment";
-import {crcTrustChangedEmailTemplate} from "./templates/crcTrustChangedEmailTemplate";
+import { JobWorker, JobWorkerConfiguration } from "../jobWorker";
+import { SendCrcTrustChangedEmail } from "../../descriptions/emailNotifications/sendCrcTrustChangedEmail";
+import { Mailer } from "../../../mailer/mailer";
+import { ProfileLoader } from "../../../querySources/profileLoader";
+import { Environment } from "../../../environment";
+import { crcTrustChangedEmailTemplate } from "./templates/crcTrustChangedEmailTemplate";
 
 export class SendCrcTrustChangedEmailWorker extends JobWorker<SendCrcTrustChangedEmail> {
   name(): string {
     return "SendCrcTrustChangedEmailWorker";
   }
 
-  constructor(configuration?:JobWorkerConfiguration) {
+  constructor(configuration?: JobWorkerConfiguration) {
     super(configuration);
   }
 
@@ -18,32 +18,40 @@ export class SendCrcTrustChangedEmailWorker extends JobWorker<SendCrcTrustChange
     // TODO: Use a different template when an organization trusts you
     if (job.limit == 0) {
       return {
-        info: `Doesn't send a notification for removed trust at the moment.`
-      }
+        info: `Doesn't send a notification for removed trust at the moment.`,
+      };
     }
 
-    const profiles = await (new ProfileLoader()
-      .profilesBySafeAddress(Environment.readonlyApiDb, [job.user, job.canSendTo]));
+    const profiles = await new ProfileLoader().profilesBySafeAddress(
+      Environment.readonlyApiDb,
+      [job.user, job.canSendTo]
+    );
 
     const user = profiles[job.user];
     const canSendTo = profiles[job.canSendTo];
 
     if (!user?.emailAddress || !user?.emailAddressVerified) {
       return {
-        info: `Couldn't send a notification email to profile ${profiles[job.user]?.id} because it has no verified email address.`
+        info: `Couldn't send a notification email to profile ${
+          profiles[job.user]?.id
+        } because it has no verified email address.`,
       };
     }
     if (!canSendTo?.circlesAddress) {
       return {
-        warning: `Couldn't send a notification email for transaction ${job.hash} because no 'canSendTo' profile could be loaded.`
+        warning: `Couldn't send a notification email for transaction ${job.hash} because no 'canSendTo' profile could be loaded.`,
       };
     }
 
-    await Mailer.send(crcTrustChangedEmailTemplate, {
-      user: `${ProfileLoader.displayName(user)}`,
-      canSendTo: `${ProfileLoader.displayName(canSendTo)}`,
-      canSendToProfileUrl: `${Environment.appUrl}#/contacts/profile/${canSendTo.circlesAddress}`
-    }, user.emailAddress);
+    await Mailer.send(
+      crcTrustChangedEmailTemplate,
+      {
+        user: `${user.firstName}`,
+        canSendTo: `${ProfileLoader.displayName(canSendTo)}`,
+        canSendToProfileUrl: `${Environment.appUrl}#/contacts/profile/${canSendTo.circlesAddress}`,
+      },
+      user.emailAddress
+    );
 
     return undefined;
   }
