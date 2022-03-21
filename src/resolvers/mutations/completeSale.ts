333599c2607f4b8c8100450f4e90aef867871adf
Thorsten <letsgo@codeho.net>
2022-03-21 18:43:44 +0100

    add createdAt date to invoice objects

--- src/resolvers/mutations/completeSale.ts
+++ src/resolvers/mutations/completeSale.ts
@@ -1,16 +1,20 @@
-import {canAccess} from "../../utils/canAccess";
-import {ProfileLoader} from "../../querySources/profileLoader";
-import {MutationCompleteSaleArgs} from "../../types";
-import {Context} from "../../context";
-import {Environment} from "../../environment";
+import { canAccess } from "../../utils/canAccess";
+import { ProfileLoader } from "../../querySources/profileLoader";
+import { MutationCompleteSaleArgs, Invoice } from "../../types";
+import { Context } from "../../context";
+import { Environment } from "../../environment";
 
-export const completeSale = async (parent:any, args:MutationCompleteSaleArgs, context:Context) => {
+export const completeSale = async (
+  parent: any,
+  args: MutationCompleteSaleArgs,
+  context: Context
+) => {
   if (!context.session?.profileId) {
     throw new Error(`You must have a profile to use this function.`);
   }
   let invoice = await Environment.readonlyApiDb.invoice.findFirst({
     where: {
-      id: args.invoiceId
+      id: args.invoiceId,
     },
     include: {
       sellerProfile: true,
@@ -20,52 +24,58 @@ export const completeSale = async (parent:any, args:MutationCompleteSaleArgs, co
         include: {
           product: {
             include: {
-              createdBy: true
-            }
-          }
-        }
-      }
-    }
+              createdBy: true,
+            },
+          },
+        },
+      },
+    },
   });
 
   if (!invoice) {
     throw new Error(`Couldn't find a invoice with id ${args.invoiceId}.`);
   }
   if (!invoice.sellerProfile.circlesAddress) {
-    throw new Error(`The seller profile of invoice ${invoice.id} has no safe address.`)
+    throw new Error(
+      `The seller profile of invoice ${invoice.id} has no safe address.`
+    );
   }
-  let canActAsOrganisation = await canAccess(context, invoice.sellerProfile.circlesAddress);
+  let canActAsOrganisation = await canAccess(
+    context,
+    invoice.sellerProfile.circlesAddress
+  );
   if (!canActAsOrganisation) {
     throw new Error(`Couldn't find a invoice with id ${args.invoiceId}.`);
   }
   if (invoice.buyerSignature && invoice.sellerSignature) {
-    throw new Error(`The purchase is already accepted by both parties.`)
+    throw new Error(`The purchase is already accepted by both parties.`);
   }
   const data = {
     sellerSignature: !args.revoke,
-    sellerSignedDate: !args.revoke ? new Date() : null
+    sellerSignedDate: !args.revoke ? new Date() : null,
   };
   await Environment.readWriteApiDb.invoice.update({
     where: {
-      id: args.invoiceId
+      id: args.invoiceId,
     },
-    data: data
+    data: data,
   });
   invoice.sellerSignature = data.sellerSignature;
   invoice.sellerSignedDate = data.sellerSignedDate;
 
-  return {
+  return <Invoice>{
     ...invoice,
     buyerAddress: invoice.customerProfile.circlesAddress ?? "",
     sellerAddress: invoice.sellerProfile.circlesAddress ?? "",
     buyerProfile: ProfileLoader.withDisplayCurrency(invoice.customerProfile),
     sellerProfile: ProfileLoader.withDisplayCurrency(invoice.sellerProfile),
-    buyerSignedDate: invoice.buyerSignedDate?.toJSON(),
-    sellerSignedDate: invoice.sellerSignedDate?.toJSON(),
-    cancelledAt: invoice.cancelledAt?.toJSON(),
+    buyerSignedDate: invoice.buyerSignedDate?.toJSON() ?? null,
+    sellerSignedDate: invoice.sellerSignedDate?.toJSON() ?? null,
+    cancelledAt: invoice.cancelledAt?.toJSON() ?? null,
+    createdAt: invoice.createdAt?.toJSON() ?? null,
     cancelledBy: ProfileLoader.withDisplayCurrency(invoice.cancelledBy),
     cancelReason: invoice.cancelReason,
-    lines: invoice.lines.map(l => {
+    lines: invoice.lines.map((l) => {
       return {
         ...l,
         offer: {
@@ -74,9 +84,11 @@ export const completeSale = async (parent:any, args:MutationCompleteSaleArgs, co
           pictureMimeType: l.product.pictureMimeType ?? "",
           createdAt: l.product.createdAt.toJSON(),
           createdByAddress: l.product.createdBy.circlesAddress ?? "",
-          createdByProfile: ProfileLoader.withDisplayCurrency(l.product.createdBy)
-        }
-      }
-    })
+          createdByProfile: ProfileLoader.withDisplayCurrency(
+            l.product.createdBy
+          ),
+        },
+      };
+    }),
   };
 };
--- src/resolvers/mutations/mutationResolvers.ts
+++ src/resolvers/mutations/mutationResolvers.ts
@@ -1,38 +1,40 @@
-import {upsertProfileResolver} from "./upsertProfile";
-import {logout} from "./logout";
-import {authenticateAtResolver} from "./authenticateAt";
-import {consumeDepositedChallengeResolver} from "./consumeDepositedChallenge";
-import {requestUpdateSafe} from "./requestUpdateSafe";
-import {updateSafe} from "./updateSafe";
-import {upsertTag} from "./upsertTag";
-import {sendMessage} from "./sendMessage";
-import {tagTransaction} from "./tagTransaction";
-import {acknowledge} from "./acknowledge";
-import {claimInvitation} from "./claimInvitation";
-import {redeemClaimedInvitation} from "./redeemClaimedInvitation";
-import {verifySessionChallengeResolver} from "./verifySessionChallengeResolver";
-import {upsertOrganisation} from "./upsertOrganisation";
-import {createTestInvitation} from "./createTestInvitation";
-import {addMemberResolver} from "./addMember";
-import {removeMemberResolver} from "./removeMember";
-import {purchaseResolver} from "./purchase";
-import {requestSessionChallenge} from "./requestSessionChallenge";
-import {importOrganisationsOfAccount} from "./importOrganisationsOfAccount";
-import {completePurchase} from "./completePurchase";
-import {completeSale} from "./completeSale";
-import {revokeSafeVerification, verifySafe} from "./verifySafe";
-import {announcePayment} from "./announcePayment";
-import {Environment} from "../../environment";
-import {MutationResolvers} from "../../types";
+import { upsertProfileResolver } from "./upsertProfile";
+import { logout } from "./logout";
+import { authenticateAtResolver } from "./authenticateAt";
+import { consumeDepositedChallengeResolver } from "./consumeDepositedChallenge";
+import { requestUpdateSafe } from "./requestUpdateSafe";
+import { updateSafe } from "./updateSafe";
+import { upsertTag } from "./upsertTag";
+import { sendMessage } from "./sendMessage";
+import { tagTransaction } from "./tagTransaction";
+import { acknowledge } from "./acknowledge";
+import { claimInvitation } from "./claimInvitation";
+import { redeemClaimedInvitation } from "./redeemClaimedInvitation";
+import { verifySessionChallengeResolver } from "./verifySessionChallengeResolver";
+import { upsertOrganisation } from "./upsertOrganisation";
+import { createTestInvitation } from "./createTestInvitation";
+import { addMemberResolver } from "./addMember";
+import { removeMemberResolver } from "./removeMember";
+import { purchaseResolver } from "./purchase";
+import { requestSessionChallenge } from "./requestSessionChallenge";
+import { importOrganisationsOfAccount } from "./importOrganisationsOfAccount";
+import { completePurchase } from "./completePurchase";
+import { completeSale } from "./completeSale";
+import { revokeSafeVerification, verifySafe } from "./verifySafe";
+import { announcePayment } from "./announcePayment";
+import { Environment } from "../../environment";
+import { MutationResolvers } from "../../types";
 
-export const mutationResolvers : MutationResolvers = {
+export const mutationResolvers: MutationResolvers = {
   purchase: purchaseResolver,
   upsertOrganisation: upsertOrganisation(false),
   upsertRegion: upsertOrganisation(true),
   logout: logout(),
   upsertProfile: upsertProfileResolver(),
   authenticateAt: authenticateAtResolver(),
-  consumeDepositedChallenge: consumeDepositedChallengeResolver(Environment.readWriteApiDb),
+  consumeDepositedChallenge: consumeDepositedChallengeResolver(
+    Environment.readWriteApiDb
+  ),
   requestUpdateSafe: requestUpdateSafe(Environment.readWriteApiDb),
   updateSafe: updateSafe(Environment.readWriteApiDb),
   upsertTag: upsertTag(),
@@ -42,7 +44,9 @@ export const mutationResolvers : MutationResolvers = {
   claimInvitation: claimInvitation(),
   redeemClaimedInvitation: redeemClaimedInvitation(),
   requestSessionChallenge: requestSessionChallenge,
-  verifySessionChallenge: verifySessionChallengeResolver(Environment.readWriteApiDb),
+  verifySessionChallenge: verifySessionChallengeResolver(
+    Environment.readWriteApiDb
+  ),
   createTestInvitation: createTestInvitation(),
   addMember: addMemberResolver,
   removeMember: removeMemberResolver,
@@ -51,5 +55,5 @@ export const mutationResolvers : MutationResolvers = {
   completeSale: completeSale,
   verifySafe: verifySafe,
   revokeSafeVerification: revokeSafeVerification,
-  announcePayment: announcePayment()
-}
+  announcePayment: announcePayment(),
+};
