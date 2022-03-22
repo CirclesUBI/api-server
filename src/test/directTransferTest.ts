5fa3aa74e2d481e9433ed5a80572688abc66a691
jaensen <daniel@circles.name>
2022-03-22 15:21:45 +0100

    added the Date type to the gql schema

---
+++ src/test/directTransferTest.ts
@@ -0,0 +1,8 @@
+describe("direct transfer of circles", () => {
+  it("should send the receiver's own tokens first", () => {
+  });
+  it("then should send all other holdings except the own", () => {
+  });
+  it("at last is should send the own tokens", () => {
+  });
+});
