02244820d23cb15c8cd75beb797fd7fab390335e
jaensen <daniel@circles.name>
2022-07-15 17:49:36 +0200

    added the allProfiles query

--- src/resolvers/queries/queryResolvers.ts
+++ src/resolvers/queries/queryResolvers.ts
@@ -100,5 +100,57 @@ export const queryResolvers: QueryResolvers = {
   getAvailableLanguages: getAvailableLanguages,
   getAllStringsByMaxVersion: getAllStringsByMaxVersion,
   getAllStringsByMaxVersionAndLang: getAllStringsByMaxVersionAndLang,
-  getOlderVersionsByKeyAndLang: getOlderVersionsByKeyAndLang
-};
+  getOlderVersionsByKeyAndLang: getOlderVersionsByKeyAndLang,
+  allProfiles: async (parent, args, context) => {
+    const profilesSql = `
+      select "circlesAddress", "firstName", "lastName", "avatarUrl"
+      from "Profile"
+      where "circlesAddress" is not null;`;
+
+    const profilesRows = await Environment.pgReadWriteApiDb.query(profilesSql);
+    const profileAddresses = profilesRows.rows.map(o => o.circlesAddress);
+    const profileLookup = profilesRows.rows.toLookup(o => o.circlesAddress, o => o);
+
+    const trustRelationsSql = `select "can_send_to" as truster_address,
+                                      "user"        as trustee_address,
+                                      "limit"       as trust_limit,
+                                      last_change
+                               from crc_current_trust_2
+                               where "user" = ANY ($1);`;
+
+
+    function getDisplayName(address: string): string {
+      let profileData = profileLookup[address];
+      if (!profileData)
+        return "";
+
+      let displayName = profileData.firstName;
+      if (profileData.lastName) {
+        displayName += ` ${profileData.lastName}`;
+      }
+      return displayName;
+    }
+
+    function getAvatarUrl(address: string): string {
+      return (profileLookup[address])
+          ? profileLookup[address].avatarUrl ?? ""
+          : "";
+    }
+
+    const trustRelationRows = await Environment.indexDb.query(trustRelationsSql, [profileAddresses]);
+    const result = trustRelationRows.rows.map(o => {
+
+      const truster_name = getDisplayName(o.truster_address);
+      const truster_image = getAvatarUrl(o.truster_address);
+
+      const trustee_name = getDisplayName(o.trustee_address);
+      const trustee_image = getAvatarUrl(o.trustee_address);
+
+      return `${o.truster_address},${truster_name},${truster_image},${o.trustee_address},${trustee_name},${trustee_image},${o.limit}`;
+    }).join('\n');
+
+    const header = `truster_address,truster_name,truster_image_url,trustee_address,trustee_name,truster_image_url,amount,block_number,limit\n`;
+    const all = header + result;
+    return all;
+  }
+}
