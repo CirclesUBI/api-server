09ada1e7d5c5656a9e6374bcbf76db33d64d0d70
Thorsten Schau <letsgo@codeho.net>
2022-11-30 13:00:43 +0100

    Fix META meta tags for sharing.

--- src/httpHandlers/get/link.ts
+++ src/httpHandlers/get/link.ts
@@ -1,15 +1,18 @@
-import {Request, Response} from "express";
-import {Environment} from "../../environment";
-import {log} from "../../utils/log";
-import {allBusinesses} from "../../resolvers/queries/allBusinesses";
-import {Context} from "../../context";
-import {ProfileLoader} from "../../querySources/profileLoader";
-import {getDisplayName} from "../../utils/getDisplayName";
+import { Request, Response } from "express";
+import { Environment } from "../../environment";
+import { log } from "../../utils/log";
+import { allBusinesses } from "../../resolvers/queries/allBusinesses";
+import { Context } from "../../context";
+import { ProfileLoader } from "../../querySources/profileLoader";
+import { getDisplayName } from "../../utils/getDisplayName";
 
-function onError(req: Request, res: Response, e:Error) {
-  log(`ERR  `,
+function onError(req: Request, res: Response, e: Error) {
+  log(
+    `ERR  `,
     `[${req.ip}; ${req.headers["user-agent"]}] [hash: ${req.query.hash}] [linkGetHandler]`,
-    `The link preview couldn't be generated:`, e);
+    `The link preview couldn't be generated:`,
+    e
+  );
 
   return res.json({
     status: "error",
@@ -30,49 +33,57 @@ export const linkGetHandler = async (req: Request, res: Response) => {
     res.statusCode = 200;
 
     let id = req.query.id?.toString();
-    if (!id)
-      return onError(req, res, new Error(`The 'id' query parameter is not set`));
+    if (!id) return onError(req, res, new Error(`The 'id' query parameter is not set`));
 
     const linkObj = await Environment.readonlyApiDb.link.findUnique({
       where: {
-        id: id
-      }
+        id: id,
+      },
     });
-    if (!linkObj)
-      return onError(req, res, new Error(`Couldn't find a link with the specified id`));
+    if (!linkObj) return onError(req, res, new Error(`Couldn't find a link with the specified id`));
 
     let document = "";
 
     switch (linkObj.linkTargetType) {
       case "Business":
-        const businesses = await allBusinesses(null, {
-          queryParams: {
-            where: {
-              inCirclesAddress: [linkObj.linkTargetKey]
-            }
-          }
-        }, new Context(id, false));
+        const businesses = await allBusinesses(
+          null,
+          {
+            queryParams: {
+              where: {
+                inCirclesAddress: [linkObj.linkTargetKey],
+              },
+            },
+          },
+          new Context(id, false)
+        );
         if (businesses.length != 1)
-          return onError(req, res, new Error(`Couldn't find a 'Business' with the specified 'circlesAddress' in the 'key' field`));
+          return onError(
+            req,
+            res,
+            new Error(`Couldn't find a 'Business' with the specified 'circlesAddress' in the 'key' field`)
+          );
 
         const business = businesses[0];
-        document +=`<!DOCTYPE html>
+        document += `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>${business.name}</title>
-</head>
-<body>
     <meta name="title" property="og:title" content="${business.name}" />
     <meta name="type" property="og:type" content="website" />
     <meta name="image" property="og:image" content="${business.picture}" />
     <meta name="url" property="og:url" content="${Environment.appUrl}#/market/detail/${business.circlesAddress}" />
     <meta name="description" property="og:description" content="${business.description}" />
+</head>
+<body>
 </body>
 </html>`;
         break;
       case "Person":
-        const persons = await new ProfileLoader().profilesBySafeAddress(Environment.readonlyApiDb, [linkObj.linkTargetKey]);
+        const persons = await new ProfileLoader().profilesBySafeAddress(Environment.readonlyApiDb, [
+          linkObj.linkTargetKey,
+        ]);
         if (Object.keys(persons).length != 1)
           return onError(req, res, new Error(`Couldn't find a 'Profile' with the 'circlesAddress' in the 'key' field`));
 
@@ -81,18 +92,18 @@ export const linkGetHandler = async (req: Request, res: Response) => {
           return onError(req, res, new Error(`Couldn't find a 'Profile' with the 'circlesAddress' in the 'key' field`));
 
         const displayName = getDisplayName(person);
-        document +=`<!DOCTYPE html>
+        document += `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>${displayName}</title>
-</head>
-<body>
     <meta name="title" property="og:title" content="${displayName}" />
     <meta name="type" property="og:type" content="website" />
     <meta name="image" property="og:image" content="${person.avatarUrl}" />
     <meta name="url" property="og:url" content="${Environment.appUrl}#/contacts/profile/${person.circlesAddress}" />
     <meta name="description" property="og:description" content="${person.dream ?? person.circlesAddress}" />
+</head>
+<body>
 </body>
 </html>`;
         break;
@@ -100,21 +111,23 @@ export const linkGetHandler = async (req: Request, res: Response) => {
         return onError(req, res, new Error(`Wrong content type. This endpoint only supports 'text/html'`));
     }
 
-    log(`     `,
+    log(
+      `     `,
       `[${req.ip}; ${req.headers["user-agent"]}] [id: ${req.query.id}] [linkGetHandler]`,
-      `The link preview was successfully generated.`);
+      `The link preview was successfully generated.`
+    );
 
     res.format({
-      'text/html': function () {
+      "text/html": function () {
         res.send(document);
       },
 
       default: function () {
         // log the request and respond with 406
-        res.status(406).send('Not Acceptable')
-      }
+        res.status(406).send("Not Acceptable");
+      },
     });
   } catch (e) {
     return onError(req, res, <Error>e);
   }
-}
+};
