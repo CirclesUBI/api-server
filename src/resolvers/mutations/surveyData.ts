9637d76ffdde9c288305f562819db008a096db96
Thorsten Schau <letsgo@codeho.net>
2023-01-19 09:51:08 +0100

    survey data collection

---
+++ src/resolvers/mutations/surveyData.ts
@@ -0,0 +1,22 @@
+import { SurveyDataInput } from "../../types";
+import { Context } from "../../context";
+import { Environment } from "../../environment";
+
+export function surveyData() {
+  return async (parent: any, args: { data: SurveyDataInput }, context: Context) => {
+    const newSurvey = await Environment.readWriteApiDb.surveyData.create({
+      data: {
+        sesssionId: args.data.sessionId,
+        allConsentsGiven: args.data.allConsentsGiven,
+        userType: args.data.userType,
+        gender: args.data.gender,
+        dateOfBirth: args.data.dateOfBirth,
+      },
+    });
+
+    return {
+      success: true,
+      data: newSurvey,
+    };
+  };
+}
