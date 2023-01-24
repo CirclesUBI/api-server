18b8d0d967af0a6d3233061c070a6132fc8191fc
Thorsten Schau <letsgo@codeho.net>
2023-01-24 14:41:06 +0100

    Revert "Adding surveyData session id to profile, add unique constraint to surveyData.SessionId"
    This reverts commit 63e9b1bc67320ef2c044fdafed10b3362024ddcd.

--- src/api-db-migrations/0.1.514-add-SurveyDataSessionId.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "SurveyData" ADD CONSTRAINT "sessionid_unique" UNIQUE ("sesssionId");
-
-ALTER TABLE "public"."Profile"
-ADD COLUMN "surveyDataSessionId" text;
-ALTER TABLE "public"."Profile" ADD FOREIGN KEY ("surveyDataSessionId") REFERENCES "public"."SurveyData" ("sesssionId");
