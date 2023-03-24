b823e3480559e54eca397dac1aa26dd969909be1
jaensen <4954577+jaensen@users.noreply.github.com>
2023-03-24 22:59:02 +0100

    added a 'getNonce' and 'sendSignedTransaction' mutation to the api-server. Get 'sessionId' with getSessionInfo.

--- src/resolvers/mutations/getNonce.ts
+++ src/resolvers/mutations/getNonce.ts
@@ -4,7 +4,7 @@ import {NonceManager} from "../../nonceManager/nonceManager";
 
 export function getNonce(nonceManager: NonceManager) {
   return async (parent: any, args: MutationGetNonceArgs, context: Context) => {
-    const nextNonce = await nonceManager.getNonce(context, args.data.signature, 30);
+    const nextNonce = await nonceManager.getNonce(context, args.data.signature, 30, args.data.address ?? undefined);
     return {
         nonce: nextNonce
     };
