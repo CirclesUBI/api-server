9f3c942c67ea79fa335361bfaed53599508473d5
Thorsten Schau <thorsten@codeho.org>
2023-05-03 15:31:38 +0200

    change to update

--- src/api-db-migrations/20230502161199__update_business_categories.sql
+++ src/api-db-migrations/20230502161199__update_business_categories.sql
@@ -1,7 +1,17 @@
 -- migrate:up
 
-TRUNCATE TABLE "BusinessCategory";
-
-INSERT INTO "BusinessCategory" (name) VALUES ('dapps.o-marketlisting.pages.marketlisting.category.TokoKelontong'),('dapps.o-marketlisting.pages.marketlisting.category.MakananMinuman'),('dapps.o-marketlisting.pages.marketlisting.category.KebunPertanian'),('dapps.o-marketlisting.pages.marketlisting.category.HasilLaut'),('dapps.o-marketlisting.pages.marketlisting.category.AlatPerangkat'),('dapps.o-marketlisting.pages.marketlisting.category.Upakara'),('dapps.o-marketlisting.pages.marketlisting.category.Utilitas'),('dapps.o-marketlisting.pages.marketlisting.category.KesehatanPengobatan'),('dapps.o-marketlisting.pages.marketlisting.category.TransportasiAngkut'),('dapps.o-marketlisting.pages.marketlisting.category.PerbaikanKonstruksi'),('dapps.o-marketlisting.pages.marketlisting.category.SalonTukangCukur'),('dapps.o-marketlisting.pages.marketlisting.category.PenjahitLaundry'),('dapps.o-marketlisting.pages.marketlisting.category.PendidikanLes');
+UPDATE "BusinessCategory" SET name = 'dapps.o-marketlisting.pages.marketlisting.category.TokoKelontong' WHERE id = 1; 
+UPDATE "BusinessCategory" SET name = 'dapps.o-marketlisting.pages.marketlisting.category.MakananMinuman' WHERE id = 2; 
+UPDATE "BusinessCategory" SET name = 'dapps.o-marketlisting.pages.marketlisting.category.KebunPertanian' WHERE id = 3; 
+UPDATE "BusinessCategory" SET name = 'dapps.o-marketlisting.pages.marketlisting.category.HasilLaut' WHERE id = 4; 
+UPDATE "BusinessCategory" SET name = 'dapps.o-marketlisting.pages.marketlisting.category.AlatPerangkat' WHERE id = 5; 
+UPDATE "BusinessCategory" SET name = 'dapps.o-marketlisting.pages.marketlisting.category.Upakara' WHERE id = 6; 
+UPDATE "BusinessCategory" SET name = 'dapps.o-marketlisting.pages.marketlisting.category.Utilitas' WHERE id = 7; 
+UPDATE "BusinessCategory" SET name = 'dapps.o-marketlisting.pages.marketlisting.category.KesehatanPengobatan' WHERE id = 8; 
+UPDATE "BusinessCategory" SET name = 'dapps.o-marketlisting.pages.marketlisting.category.TransportasiAngkut' WHERE id = 9; 
+UPDATE "BusinessCategory" SET name = 'dapps.o-marketlisting.pages.marketlisting.category.PerbaikanKonstruksi' WHERE id = 10 ; 
+UPDATE "BusinessCategory" SET name = 'dapps.o-marketlisting.pages.marketlisting.category.SalonTukangCukur' WHERE id = 11; 
+UPDATE "BusinessCategory" SET name = 'dapps.o-marketlisting.pages.marketlisting.category.PenjahitLaundry' WHERE id = 12; 
+UPDATE "BusinessCategory" SET name = 'dapps.o-marketlisting.pages.marketlisting.category.PendidikanLes' WHERE id = 13; 
 
 -- migrate:down
