e892bfeb64ba95cd086dc2e5b1fddb307954ce0a
jon r <jon@gnosis.io>
2023-06-08 02:08:53 +0200

    OP-28: fix(migrations): add business categories

--- src/api-db-migrations/20230608015359__add_business_categories.sql
+++ src/api-db-migrations/20230608015359__add_business_categories.sql
@@ -1,19 +1,18 @@
 -- migrate:up
 
-COPY public."BusinessCategory" (id, name) FROM stdin;
-1       dapps.o-marketlisting.pages.marketlisting.category.TokoKelontong
-2       dapps.o-marketlisting.pages.marketlisting.category.MakananMinuman
-3       dapps.o-marketlisting.pages.marketlisting.category.KebunPertanian
-4       dapps.o-marketlisting.pages.marketlisting.category.HasilLaut
-5       dapps.o-marketlisting.pages.marketlisting.category.AlatPerangkat
-6       dapps.o-marketlisting.pages.marketlisting.category.Upakara
-7       dapps.o-marketlisting.pages.marketlisting.category.Utilitas
-8       dapps.o-marketlisting.pages.marketlisting.category.KesehatanPengobatan
-9       dapps.o-marketlisting.pages.marketlisting.category.TransportasiAngkut
-10      dapps.o-marketlisting.pages.marketlisting.category.PerbaikanKonstruksi
-11      dapps.o-marketlisting.pages.marketlisting.category.SalonTukangCukur
-12      dapps.o-marketlisting.pages.marketlisting.category.PenjahitLaundry
-13      dapps.o-marketlisting.pages.marketlisting.category.PendidikanLes
-\.
+INSERT INTO "BusinessCategory" (id, name) VALUES
+(1 , 'dapps.o-marketlisting.pages.marketlisting.category.TokoKelontong'),
+(2 , 'dapps.o-marketlisting.pages.marketlisting.category.MakananMinuman'),
+(3 , 'dapps.o-marketlisting.pages.marketlisting.category.KebunPertanian'),
+(4 , 'dapps.o-marketlisting.pages.marketlisting.category.HasilLaut'),
+(5 , 'dapps.o-marketlisting.pages.marketlisting.category.AlatPerangkat'),
+(6 , 'dapps.o-marketlisting.pages.marketlisting.category.Upakara'),
+(7 , 'dapps.o-marketlisting.pages.marketlisting.category.Utilitas'),
+(8 , 'dapps.o-marketlisting.pages.marketlisting.category.KesehatanPengobatan'),
+(9 , 'dapps.o-marketlisting.pages.marketlisting.category.TransportasiAngkut'),
+(10, 'dapps.o-marketlisting.pages.marketlisting.category.PerbaikanKonstruksi'),
+(11, 'dapps.o-marketlisting.pages.marketlisting.category.SalonTukangCukur'),
+(12, 'dapps.o-marketlisting.pages.marketlisting.category.PenjahitLaundry'),
+(13, 'dapps.o-marketlisting.pages.marketlisting.category.PendidikanLes');
 
 -- migrate:down
