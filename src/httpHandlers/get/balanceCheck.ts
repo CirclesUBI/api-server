62ed845b630451e5340cadb0b8027155a3759fd8
Ingo Collatz <incollatz@mail.de>
2023-07-18 14:18:33 +0200

    Changed response-messages

--- src/httpHandlers/get/balanceCheck.ts
+++ src/httpHandlers/get/balanceCheck.ts
@@ -9,13 +9,17 @@ const web3 = RpcGateway.get();
 
 export const balanceCheckGetHandler = async (req: Request, res: Response) => {
   const address = req.params.address;
-  const threshold = req.params.threshold;
-
-  const balance = await web3.eth.getBalance(address);
+  const threshold = parseInt(req.params.threshold, 10);
+  const balance = parseInt(await web3.eth.getBalance(address), 10);
+  const diff = threshold - balance;
 
   if (balance > threshold) {
-    return res.status(200).send("Funds are sufficient");
+    return res.status(200).send("Balance is high enough" + BigInt(balance));
   }
 
-  return res.status(400).send("Funds are insufficient. Please top up your account.");
+  return res.status(500)
+    .send(
+      "Balance is too low = " + BigInt(balance)
+      + "<br/> Threshold = " + BigInt(threshold)
+      + "<br/> Difference = " + BigInt(diff))
 };
