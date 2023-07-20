694871597c19dd8216525dab7c4be659101cbb1c
Ingo Collatz <incollatz@mail.de>
2023-07-20 10:04:13 +0200

    error handling

--- src/httpHandlers/get/balanceCheck.ts
+++ src/httpHandlers/get/balanceCheck.ts
@@ -4,22 +4,47 @@ import { Environment } from "../../environment";
 
 RpcGateway.setup(Environment.rpcGatewayUrl, Environment.fixedGasPrice);
 
-
 const web3 = RpcGateway.get();
 
 export const balanceCheckGetHandler = async (req: Request, res: Response) => {
-  const address = req.params.address;
-  const threshold = parseInt(req.params.threshold, 10);
-  const balance = parseInt(await web3.eth.getBalance(address), 10);
-  const diff = threshold - balance;
+  let address = req.params.address;
+  let threshold: bigint;
+  let balance: bigint;
+  let diff: bigint;
 
-  if (balance > threshold) {
-    return res.status(200).send("Balance is high enough" + BigInt(balance));
+  try {
+    if (isNaN(req.params.threshold as any)) {
+      throw new Error(`Threshold is not a number`);
+    }
+
+    threshold = BigInt(parseInt(req.params.threshold as string, 10));
+    balance = BigInt(parseInt(await web3.eth.getBalance(address), 10));
+    if (balance == BigInt(0)) {
+      throw new Error(`Balance is zero. Are you sure, you have the right address?`);
+    }
+    diff = threshold - balance;
+    if (balance < threshold) {
+      throw new Error(
+        `Balance is too low. Balance: ${web3.utils.fromWei(BigInt(balance).toString(), "ether")} ETH \
+        < \
+        Threshold: ${web3.utils.fromWei(BigInt(threshold).toString(), "ether")} ETH. \
+        Difference: - ${web3.utils.fromWei(BigInt(diff).toString(), "ether")} ETH`);
+    }
+    if (balance >= threshold) {
+      res.statusCode = 200;
+      return res.json({
+        status: "ok",
+        message: `Balance is high enough`,
+        balance: web3.utils.fromWei(BigInt(balance).toString(), "ether"),
+        unit: "ETH",
+      })
+    }
   }
-
-  return res.status(500)
-    .send(
-      "Balance is too low = " + BigInt(balance)
-      + "<br/> Threshold = " + BigInt(threshold)
-      + "<br/> Difference = " + BigInt(diff))
+  catch (e) {
+    res.statusCode = 500;
+    return res.json({
+      status: "error",
+      message: `${e}`,
+    });
+  }
 };
